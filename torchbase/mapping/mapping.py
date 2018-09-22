34e0a67a26a88dfb199a2f3cacc238b7459d993e
Justin Payne <justin.payne@fda.hhs.gov>
2018-09-22 17:10:12 -0400

    context manager use in mapping.py

--- torchbase/mapping/mapping.py
+++ torchbase/mapping/mapping.py
@@ -17,17 +17,18 @@ def run_srst2(tor, locus_name, fastq1, fastq2=None):
 
     :return:
     """
-    locus_file_path = tor.expose_torch_as_fasta(locus_name)
+    with tor.expose_torch_as_fasta as locus_file_path:
+    #locus_file_path = tor.expose_torch_as_fasta(locus_name)
     # adds uuid to outfile name to make distinct
-    outfile = os.path.join(os.path.dirname(locus_file_path), str(uuid.uuid1()))
+        outfile = os.path.join(os.path.dirname(locus_file_path), str(uuid.uuid1()))
 
-    if not fastq2:
-        command = ["srst2", "--input_se", fastq1, "--output", outfile, "--log", "--gene_db", locus_file_path]
-    else:
-        command = ["srst2", "--input_pe", fastq1, fastq2, "--output", outfile, "--log", "--gene_db", locus_file_path]
+        if not fastq2:
+            command = ["srst2", "--input_se", fastq1, "--output", outfile, "--log", "--gene_db", locus_file_path]
+        else:
+            command = ["srst2", "--input_pe", fastq1, fastq2, "--output", outfile, "--log", "--gene_db", locus_file_path]
 
-    process = Popen(command, stdout=PIPE, stderr=PIPE)
-    (stdout, stderr) = process.communicate()
+        process = Popen(command, stdout=PIPE, stderr=PIPE)
+        (stdout, stderr) = process.communicate()
     log.info(stdout)
     if stderr:
         log.warn(stderr)
